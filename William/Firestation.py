bda3a4d5efa43413163f300e1a4d4c6b2353a177
william0123 <w.susanto@aol.com>
2020-11-12 15:27:40 +0700

    Super Detailed

--- William/Firestation.py
+++ William/Firestation.py
@@ -1,40 +1,43 @@
 
+# Parallels Arrays
 fire_station = ["ALPHA", "BETA", "THETA","CENTER", "RAILWAY", "HARBOR", "SUBURB"]
 personnel = [12, 13, 23, 44, 23, 11, 43]
-fire_duty = []
-station_on_duty = ""
+
+# Cloning the fire stations
+fire_duty = fire_station
+
+# Finding the understaffed station
 min = personnel[0]
-understaffed = ""
-input_device = ""
-loop = 0
-
-for i in range(7):
-    fire_duty.append(fire_station[i])
-
 for m in range(7):
     if min >= personnel[m]:
         min = personnel[m]
         understaffed = fire_station[m]
 
+# Main Loop
 i = 0
-while i < 52:
-    input_device = input("true / false: ")
-    if input_device == "true":
-        if loop < 7:
-            station_on_duty = fire_duty[loop]
-            if station_on_duty == understaffed:
-                print("this station is understaffed")
-            print(station_on_duty)
-            loop += 1
-        else:
-            loop = 0
-            station_on_duty = fire_duty[loop]
-            if station_on_duty == understaffed:
-                print("this station is understaffed")
-            print(station_on_duty)
-            loop += 1
+loop = 0
 
-    elif input_device == "false":
-        i = 53
+# 1 year = 52 weeks
+while i < 52:
+
+    # Mayor's input
+    input_device = input("true / false: ")
+
+    # Input is true
+    if input_device == "true":
+
+        # Pick the station
+        station_on_duty = fire_duty[loop]
+
+        if station_on_duty == understaffed:             # Check if it is understaffed
+            print("this station is understaffed")       # If understaffed print "Station is Understaffed"
+        print(station_on_duty)                          # Printing the station on duty
+        loop += 1                                       # Prepare for next week
+
+        if loop >= 7: loop = 0                          # Resets if there are no more stations left
+
+    elif input_device == "false":                       # If mayors input is false
+        i = 53                                          # Make i = 53 to end the loop
         print("Emergency stop of procedure")
+
     i += 1
