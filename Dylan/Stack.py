e8360370aae06e0c0b7ed7fd750f20ca3922ea3a
Dy0r <73545857+Dy0r@users.noreply.github.com>
2020-12-18 15:20:38 +0700

    Stack

--- Dylan/Stack.py
+++ Dylan/Stack.py
@@ -1,36 +1,32 @@
 #Book Stack
-cn1 = ["In The Deep Blue Ocean", "The Stranger", "Polly wants a cracker"]
-cn2 = ["The Secret of Monkey Island"]
-cn3 = ["A Good Leader Is A Good Servant", "The Quest Of Rosetta", "Le Chuck's Revenge"]
-cn4 = ["The Bloody Hell Mary", "The Lost Necklace", "The Magical Season Of Christmas"]
-cn5 = ["Sherlock Holmes", "Graduation Day"]
+cn = [["In The Deep Blue Ocean", "The Stranger", "Polly wants a cracker"],
+       ["The Secret of Monkey Island"],
+       ["A Good Leader Is A Good Servant", "The Quest Of Rosetta", "Le Chuck's Revenge"],
+       ["The Bloody Hell Mary", "The Lost Necklace", "The Magical Season Of Christmas"],
+      ["Sherlock Holmes", "Graduation Day"]]
 
-#Remove Graduation and Sherlock and put graduation
-cn5.pop()
-cn5.pop()
-cn5.append("Graduation Day")
+a = True
 
-#Put Sherlock 4th bottom
-cn4.pop()
-cn4.pop()
-cn4.pop()
-cn4.append("Sherlock Holmes")
-cn4.append("The Bloody Hell Mary")
-cn4.append("The Magical Season Of Christmas")
+def container():
+        print(f"Container 1: {cn[0]}")
+        print(f"Container 2: {cn[1]}")
+        print(f"Container 3: {cn[2]}")
+        print(f"Container 4: {cn[3]}")
+        print(f"Container 5: {cn[4]}")
 
-#Put Lost necklace 1st bottom
-cn1.pop()
-cn1.pop()
-cn1.pop()
-cn1.append("The Lost Necklace")
-cn1.append("In The Deep Blue Ocean")
-cn1.append("Polly wants a cracker")
+def game():
+    print("Where's My Sherlock Holmes")
+    while a == True:
+        print("\n")
+        container()
+        move_book = int(input("\n Select a container to move the book (Number): "))
+        move_container = int(input(" Select the container to be moved to (Number): "))
+        move(move_book, move_container)
 
-#Put Stranger 2nd top
-cn2.append("The Stranger")
+def move(book, container):
+    cn[container - 1].append(cn[book - 1][-1])
+    cn[book - 1].pop()
 
-print(cn1)
-print(cn2)
-print(cn3)
-print(cn4)
-print(cn5)
+game()
+
+
