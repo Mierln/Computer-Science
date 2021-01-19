d5612dfbcac21f3e4ac5087373b98a03bbd72e69
Dy0r <73545857+Dy0r@users.noreply.github.com>
2021-01-19 09:36:03 +0700

    Hanoi + Fix Stack Sherlock

--- Dylan/Stack.py
+++ Dylan/Stack.py
@@ -5,18 +5,18 @@ cn = [["In The Deep Blue Ocean", "The Stranger", "Polly wants a cracker"],
        ["The Bloody Hell Mary", "The Lost Necklace", "The Magical Season Of Christmas"],
       ["Sherlock Holmes", "Graduation Day"]]
 
-a = True
 
 def container():
-        print(f"Container 1: {cn[0]}")
-        print(f"Container 2: {cn[1]}")
-        print(f"Container 3: {cn[2]}")
-        print(f"Container 4: {cn[3]}")
-        print(f"Container 5: {cn[4]}")
+    print(f"Container 1: {cn[0]}")
+    print(f"Container 2: {cn[1]}")
+    print(f"Container 3: {cn[2]}")
+    print(f"Container 4: {cn[3]}")
+    print(f"Container 5: {cn[4]}")
+
 
 def game():
     print("\nWhere's My Sherlock Holmes")
-    while a == True:
+    while True:
         print("\n")
         container()
 
@@ -33,10 +33,11 @@ def game():
         else:
             move(move_book, move_container)
 
+
 def move(book, container):
     cn[container - 1].append(cn[book - 1][-1])
     cn[book - 1].pop()
 
-game()
 
-
+if __name__ == '__main__':
+    game()
---
+++ Dylan/hanoi.py
@@ -0,0 +1,41 @@
+hanoi = [["*******", "*****", "***", "*"], [], []]
+answer = ["*******", "*****", "***", "*"]
+"""(1,2) (1,3) (2,3) (1,3) (1,2) (3,1) (3,2) (1,2) (1,3) (2,3) (2,1) (3,1) (2,3) (1,2) (1,3) (2,3)"""
+
+
+def container():
+    print("\n")
+    print(f"First Tower: {hanoi[0]}")
+    print(f"Second Tower: {hanoi[1]}")
+    print(f"Third Tower: {hanoi[2]}")
+
+
+def game():
+    while True:
+        container()
+        origin = int(input("\n Select a tower to move the donut (Number): "))
+        if len(hanoi[origin - 1]) == 0:
+            print("ERROR: That Tower is Empty")
+            game()
+
+        destination = int(input(" Select a tower to be moved to (Number): "))
+        if len(hanoi[destination - 1]) >= 1:
+            if len(hanoi[origin - 1][-1]) > len(hanoi[destination - 1][-1]):
+                print("ERROR: The Bigger Discs Cannot Be On Top of a Smaller One!")
+                game()
+
+        if origin == destination:
+            print(f"You Can't Move Tower {origin} to Tower {destination}")
+            game()
+
+        move(origin, destination)
+
+
+def move(origin, destination):
+    hanoi[destination - 1].append(hanoi[origin - 1][-1])
+    hanoi[origin - 1].pop()
+
+
+if __name__ == '__main__':
+    print("\nTower of Hanoi")
+    game()
